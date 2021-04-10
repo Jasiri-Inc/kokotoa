14db26bf2490647d4a61b2dffe9cf1a49c916329
brunoalfred <alfredybruno@gmail.com>
2021-04-10 18:56:59 +0300

    Made the home Screen Ready.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import './views/CalculatorHomeScreen.dart';
+import './views/calculatorHomeScreen.dart';
 
 class CalculatorApp extends StatelessWidget {
 
--- lib/src/views/calculatorHomeScreen.dart
+++ lib/src/views/calculatorHomeScreen.dart
@@ -10,25 +10,47 @@ class CalculatorHomePage extends StatefulWidget {
 
   final String title;
 
+
   @override
   _CalculatorHomePage createState() => _CalculatorHomePage();
 
 }
 
 class _CalculatorHomePage extends State<CalculatorHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
 
-      _counter++;
-    });
+  String _output;
+
+  @override
+  void initState() {
+
+    super.initState();
   }
+
+  @override
+  void dispose() {
+
+
+    super.dispose();
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
 
+
+    Size screen = MediaQuery.of(context).size;
+    double buttonSize = screen.width / 4;
+    double displayHeight = screen.height - (buttonSize * 5) - (buttonSize);
+
+
+
     return Scaffold(
+
+      backgroundColor: const Color(0xff212121 ),
+
       appBar: AppBar(
 
         title: Text(widget.title),
@@ -36,20 +58,14 @@ class _CalculatorHomePage extends State<CalculatorHomePage> {
       body: Center(
 
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+         mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => (print('yes')),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
