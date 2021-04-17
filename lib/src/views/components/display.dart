74e6e5084f3d14dccbae73c4a5dfd6c92a6fb288
brunoalfred <alfredybruno@gmail.com>
2021-04-18 00:47:51 +0300

    defined the app dispaly part to show all the operands and results.

--- lib/src/views/components/display.dart
+++ lib/src/views/components/display.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 
 class Display extends StatelessWidget {
+  final String value;
+  final double height;
+
+  Display({Key key, this.height, this.value}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.headline3.copyWith();
+
     return Container(
-      margin: const EdgeInsets.all(10.0),
-      color: Colors.amber[600],
-      width: 48.0,
-      height: 48.0,
+      width: 381,
+      height: 209,
+      margin: const EdgeInsets.all(30.0),
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color.fromRGBO(253, 134, 56, 1.0),
+          width: 1, //                   <--- border width here
+        ),
+        borderRadius: new BorderRadius.all(const Radius.circular(20.0)),
+      ),
+      //
+      //             <--- BoxDecoration here
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                'data',
+                style: textStyle,
+              ),
+            ],
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                'data',
+                style: textStyle,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
