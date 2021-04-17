0c461f7ba35547d3aca18dc8077bdd1e0d1e2a24
brunoalfred <alfredybruno@gmail.com>
2021-04-18 01:18:13 +0300

    (WIP)Arranged the keys on the key pad section.

--- lib/src/views/components/key_pad.dart
+++ lib/src/views/components/key_pad.dart
@@ -1,8 +1,62 @@
 import 'package:flutter/material.dart';
-
+import 'package:kokotoa/src/views/components/calculator_key.dart';
 class KeyPad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            CalculatorKey(symbol: Keys.clear),
+            CalculatorKey(symbol: Keys.sign),
+            CalculatorKey(symbol: Keys.percent),
+            CalculatorKey(symbol: Keys.divide),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            CalculatorKey(symbol: Keys.clear),
+            CalculatorKey(symbol: Keys.sign),
+            CalculatorKey(symbol: Keys.percent),
+            CalculatorKey(symbol: Keys.divide),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            CalculatorKey(symbol: Keys.clear),
+            CalculatorKey(symbol: Keys.sign),
+            CalculatorKey(symbol: Keys.percent),
+            CalculatorKey(symbol: Keys.divide),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CalculatorKey(symbol: Keys.clear),
+                    CalculatorKey(symbol: Keys.sign),
+                    CalculatorKey(symbol: Keys.percent),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    CalculatorKey(symbol: Keys.clear),
+                    CalculatorKey(symbol: Keys.sign),
+                    CalculatorKey(symbol: Keys.percent),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                CalculatorKey(symbol: Keys.clear),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
