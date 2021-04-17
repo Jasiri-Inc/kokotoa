b29fd0f49e4aafc6f152628dc414c1daa6711c28
brunoalfred <alfredybruno@gmail.com>
2021-04-17 08:40:41 +0300

    Added app back icons

--- lib/src/views/screens/home_screen.dart
+++ lib/src/views/screens/home_screen.dart
@@ -12,9 +12,32 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.blue,
       appBar: AppBar(
         centerTitle: true,
-        title: Text('calculator', ),
+        title: Text(
+          'calculator',
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            print('Clicked');
+          },
+          child: Icon(Icons.menu),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                print('clicked');
+              },
+              child: Icon(Icons.ac_unit_sharp),
+            ),
+          )
+        ],
+        actionsIconTheme: IconThemeData(
+          size: 30.0,
+          color: Colors.deepOrange,
+          opacity: 10.0,
+        ),
       ),
-
     );
   }
 }
