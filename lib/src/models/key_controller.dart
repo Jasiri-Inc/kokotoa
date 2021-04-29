2fe5a3d4bc73d145642628a7009e19d00fa813f4
brunoalfred <alfredybruno@gmail.com>
2021-04-29 21:16:54 +0300

    set up the key controller class to respond to the user input...

---
+++ lib/src/models/key_controller.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:kokotoa/src/views/components/calculator_key.dart';
+
+class KeyEvent{
+
+  final CalculatorKey key;
+
+  KeyEvent(this.key);
+
+}
+
+
+abstract class KeyController{
+
+
+  static StreamController _controller = StreamController();
+  static Stream get _stream => _controller.stream;
+
+  static StreamSubscription listen(Function handler) => _stream.listen(handler as dynamic);
+  static void fire(KeyEvent event) => _controller.add(event);
+
+
+  static dispose() => _controller.close();
+
+}
